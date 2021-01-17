d03cb7a6e0be0403a68c996a77e90497d028874e
Sarfaraz Kasmani <sarfarazkasmani@Sarfarazs-MacBook-Air.local>
2021-01-17 07:36:50 +0530

    first commit

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,8 @@
 import React from "react"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+// Components
+import Layout from "components/Layout"
+import SEO from "components/SEO"
 
 const NotFoundPage = () => (
   <Layout>
